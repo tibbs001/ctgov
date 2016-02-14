75535691a888c4a772ae702a0b726b00efc98cfc
Sheri Tibbs <sheri.tibbs@duke.edu>
2016-02-14 19:37:21 +0000

    fix migration that creates user table

--- db/migrate/20160214191640_add_devise_to_users.rb
+++ db/migrate/20160214191640_add_devise_to_users.rb
@@ -1,6 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
-  def self.up
-    change_table(:users) do |t|
+  def change
+    create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -41,9 +41,4 @@ class AddDeviseToUsers < ActiveRecord::Migration
     # add_index :users, :unlock_token,         unique: true
   end
 
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
-  end
 end
